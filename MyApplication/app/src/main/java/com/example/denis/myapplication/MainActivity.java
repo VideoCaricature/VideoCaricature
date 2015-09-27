ded9ca4d044ac163c118bd6f22f4de72152fde97
amvasil <vasil94@inbox.ru>
2015-09-27 20:09:19 +0300

    Added face detection via OpenCV defaut Haar Cascade. Amount of faces recognised is displayed at TextView.

--- MyApplication/app/src/main/java/com/example/denis/myapplication/MainActivity.java
+++ MyApplication/app/src/main/java/com/example/denis/myapplication/MainActivity.java
@@ -1,7 +1,9 @@
 package com.example.denis.myapplication;
 
+import android.content.Context;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
 
@@ -9,17 +11,76 @@ import android.app.Activity;
 import android.os.Bundle;
 import android.view.SurfaceView;
 import android.view.WindowManager;
+import android.widget.TextView;
 
 
+import org.opencv.android.BaseLoaderCallback;
 import org.opencv.android.CameraBridgeViewBase;
 import org.opencv.android.CameraBridgeViewBase.CvCameraViewFrame;
 import org.opencv.android.CameraBridgeViewBase.CvCameraViewListener2;
+import org.opencv.android.LoaderCallbackInterface;
 import org.opencv.android.OpenCVLoader;
+import org.opencv.core.*;
 import org.opencv.core.Mat;
+import org.opencv.objdetect.CascadeClassifier;
+import org.w3c.dom.Text;
+
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.InputStream;
 
 
 public class MainActivity extends Activity implements CvCameraViewListener2 {
     private CameraBridgeViewBase mOpenCvCameraView;
+    private Mat grayscaleImage;
+    private CascadeClassifier cascadeClassifier;
+    private int absoluteFaceSize;
+    private String text_str;
+
+    private BaseLoaderCallback mLoaderCallback = new BaseLoaderCallback(this) {
+        @Override
+        public void onManagerConnected(int status) {
+            switch (status) {
+                case LoaderCallbackInterface.SUCCESS:
+                    initializeOpenCVDependencies();
+                    break;
+                default:
+                    super.onManagerConnected(status);
+                    break;
+            }
+        }
+    };
+
+    private void initializeOpenCVDependencies() {
+
+
+        try {
+            // Copy the resource into a temp file so OpenCV can load it
+            InputStream is = getResources().openRawResource(R.raw.lbpcascade_frontalface);
+            File cascadeDir = getDir("cascade", Context.MODE_PRIVATE);
+            File mCascadeFile = new File(cascadeDir, "lbpcascade_frontalface.xml");
+            FileOutputStream os = new FileOutputStream(mCascadeFile);
+
+
+            byte[] buffer = new byte[4096];
+            int bytesRead;
+            while ((bytesRead = is.read(buffer)) != -1) {
+                os.write(buffer, 0, bytesRead);
+            }
+            is.close();
+            os.close();
+
+
+            // Load the cascade classifier
+            cascadeClassifier = new CascadeClassifier(mCascadeFile.getAbsolutePath());
+        } catch (Exception e) {
+            Log.e("OpenCVActivity", "Error loading cascade", e);
+        }
+
+
+        // And we are ready to go
+        mOpenCvCameraView.enableView();
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -39,12 +100,18 @@ public class MainActivity extends Activity implements CvCameraViewListener2 {
             mOpenCvCameraView.disableView();
     }
 
-    @Override
+    /*@Override
     public void onResume()
     {
         super.onResume();
-        OpenCVLoader.initDebug();
-        mOpenCvCameraView.enableView();
+        //OpenCVLoader.initDebug();
+        //mOpenCvCameraView.enableView();
+    }*/
+
+    @Override
+    public void onResume() {
+        super.onResume();
+        OpenCVLoader.initAsync(OpenCVLoader.OPENCV_VERSION_2_4_6, this, mLoaderCallback);
     }
 
     public void onDestroy() {
@@ -54,12 +121,40 @@ public class MainActivity extends Activity implements CvCameraViewListener2 {
     }
 
     public void onCameraViewStarted(int width, int height) {
+        absoluteFaceSize = (int) (height * 0.2);
     }
 
     public void onCameraViewStopped() {
     }
 
     public Mat onCameraFrame(CvCameraViewFrame inputFrame) {
+
+        grayscaleImage = inputFrame.gray();
+        MatOfRect faces = new MatOfRect();
+
+
+        // Use the classifier to detect faces
+        if (cascadeClassifier != null) {
+            cascadeClassifier.detectMultiScale(grayscaleImage, faces, 1.1, 2, 2,
+                    new Size(absoluteFaceSize, absoluteFaceSize), new Size());
+        }
+
+
+        // If there are any faces found, draw a rectangle around it
+        Rect[] facesArray = faces.toArray();
+        String new_text_str = String.valueOf(facesArray.length);
+        if(!new_text_str.equals(text_str))        {
+            text_str = new_text_str;
+            this.runOnUiThread(new Runnable() {
+                @Override
+                public void run() {
+                    TextView textView = (TextView)(findViewById(R.id.textView));
+                    textView.setText(text_str);
+                }
+            });
+        }
+        /*for (int i = 0; i <facesArray.length; i++)
+            Core.rectangle(inputFrame, facesArray[i].tl(), facesArray[i].br(), new Scalar(0, 255, 0, 255), 3);*/
         return inputFrame.rgba();
     }
 }
