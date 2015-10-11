ef87739a051189bd28f1d9a4153b7ec1ba687225
amvasil <vasil94@inbox.ru>
2015-10-11 18:10:14 +0300

    Added interface and class for face recognition

---
+++ MyApplication/app/src/main/java/com/example/denis/myapplication/IDetector.java
@@ -0,0 +1,39 @@
+package com.example.denis.myapplication;
+
+import org.opencv.core.Mat;
+import org.opencv.core.Rect;
+
+/**
+ * Created by vasil on 11.10.2015.
+ *
+ * This interface contains methods essential for face detection
+ * OpenCV detector and API detector must implement this interface
+ */
+public interface IDetector {
+
+    /*
+    Method called for every frame to set up a picture for detector
+     */
+    public void setCameraFrame(Mat mat);
+
+    /*
+    Method is called after void setCameraFrame(Mat mat); to detect all faces
+     */
+    public void detect();
+
+    /*
+    Method returns number of recognised faces
+     */
+    public int getFacesCount();
+
+    /*
+    Must return the biggest face rectangle avaliable
+    or null, if no faces were detected
+     */
+    public Rect getNearestFaceRectange();
+
+    /*
+    Used to set face size for detector to work properly
+     */
+    public void setAbsoluteFaceSize(int size);
+}
--- MyApplication/app/src/main/java/com/example/denis/myapplication/MainActivity.java
+++ MyApplication/app/src/main/java/com/example/denis/myapplication/MainActivity.java
@@ -1,6 +1,9 @@
 package com.example.denis.myapplication;
 
 import android.content.Context;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.media.FaceDetector;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
@@ -11,6 +14,7 @@ import android.app.Activity;
 import android.os.Bundle;
 import android.view.SurfaceView;
 import android.view.WindowManager;
+import android.widget.ImageView;
 import android.widget.TextView;
 
 
@@ -18,8 +22,10 @@ import org.opencv.android.BaseLoaderCallback;
 import org.opencv.android.CameraBridgeViewBase;
 import org.opencv.android.CameraBridgeViewBase.CvCameraViewFrame;
 import org.opencv.android.CameraBridgeViewBase.CvCameraViewListener2;
+import org.opencv.android.JavaCameraView;
 import org.opencv.android.LoaderCallbackInterface;
 import org.opencv.android.OpenCVLoader;
+import org.opencv.android.Utils;
 import org.opencv.core.*;
 import org.opencv.core.Mat;
 import org.opencv.objdetect.CascadeClassifier;
@@ -29,14 +35,19 @@ import java.io.File;
 import java.io.FileOutputStream;
 import java.io.InputStream;
 
+import static android.graphics.Bitmap.Config.RGB_565;
+
 
 public class MainActivity extends Activity implements CvCameraViewListener2 {
-    private CameraBridgeViewBase mOpenCvCameraView;
-    private Mat grayscaleImage;
-    private CascadeClassifier cascadeClassifier;
-    private int absoluteFaceSize;
+    private CameraBridgeViewDrawer mOpenCvCameraView;
+    private OpenCvFaceDetector detector;
     private String text_str;
+    private String api_text;
 
+
+    /*
+    Connect to OpenCVManager
+     */
     private BaseLoaderCallback mLoaderCallback = new BaseLoaderCallback(this) {
         @Override
         public void onManagerConnected(int status) {
@@ -51,8 +62,10 @@ public class MainActivity extends Activity implements CvCameraViewListener2 {
         }
     };
 
+    /*
+    Load data required for OpenCV Haar cascade
+     */
     private void initializeOpenCVDependencies() {
-
 
         try {
             // Copy the resource into a temp file so OpenCV can load it
@@ -60,6 +73,7 @@ public class MainActivity extends Activity implements CvCameraViewListener2 {
             File cascadeDir = getDir("cascade", Context.MODE_PRIVATE);
             File mCascadeFile = new File(cascadeDir, "lbpcascade_frontalface.xml");
             FileOutputStream os = new FileOutputStream(mCascadeFile);
+
 
 
             byte[] buffer = new byte[4096];
@@ -70,12 +84,18 @@ public class MainActivity extends Activity implements CvCameraViewListener2 {
             is.close();
             os.close();
 
-
+            detector = new OpenCvFaceDetector();
             // Load the cascade classifier
-            cascadeClassifier = new CascadeClassifier(mCascadeFile.getAbsolutePath());
+            if(!detector.loadCascadeClassifier(mCascadeFile))
+            {
+                Log.e("OpenCVActivity","Failed to load cascade from file");
+            }
+
         } catch (Exception e) {
             Log.e("OpenCVActivity", "Error loading cascade", e);
+
         }
+
 
 
         // And we are ready to go
@@ -87,9 +107,12 @@ public class MainActivity extends Activity implements CvCameraViewListener2 {
         super.onCreate(savedInstanceState);
         getWindow().addFlags(WindowManager.LayoutParams.FLAG_KEEP_SCREEN_ON);
         setContentView(R.layout.activity_main);
-        mOpenCvCameraView = (CameraBridgeViewBase) findViewById(R.id.view);
+        mOpenCvCameraView = (CameraBridgeViewDrawer) findViewById(R.id.view);
         mOpenCvCameraView.setVisibility(SurfaceView.VISIBLE);
         mOpenCvCameraView.setCvCameraViewListener(this);
+
+        Bitmap bitmap = BitmapFactory.decodeResource(getResources(),R.drawable.pict);
+        mOpenCvCameraView.setBitmap(bitmap);
     }
 
     @Override
@@ -121,7 +144,7 @@ public class MainActivity extends Activity implements CvCameraViewListener2 {
     }
 
     public void onCameraViewStarted(int width, int height) {
-        absoluteFaceSize = (int) (height * 0.2);
+        detector.setAbsoluteFaceSize((int) (height * 0.2));
     }
 
     public void onCameraViewStopped() {
@@ -129,20 +152,15 @@ public class MainActivity extends Activity implements CvCameraViewListener2 {
 
     public Mat onCameraFrame(CvCameraViewFrame inputFrame) {
 
-        grayscaleImage = inputFrame.gray();
-        MatOfRect faces = new MatOfRect();
+        detector.setCameraFrame(inputFrame.gray());
 
 
         // Use the classifier to detect faces
-        if (cascadeClassifier != null) {
-            cascadeClassifier.detectMultiScale(grayscaleImage, faces, 1.1, 2, 2,
-                    new Size(absoluteFaceSize, absoluteFaceSize), new Size());
-        }
+        detector.detect();
 
 
         // If there are any faces found, draw a rectangle around it
-        Rect[] facesArray = faces.toArray();
-        String new_text_str = String.valueOf(facesArray.length);
+        String new_text_str = String.valueOf(detector.getFacesCount());
         if(!new_text_str.equals(text_str))        {
             text_str = new_text_str;
             this.runOnUiThread(new Runnable() {
@@ -153,6 +171,10 @@ public class MainActivity extends Activity implements CvCameraViewListener2 {
                 }
             });
         }
+
+
+
+
         /*for (int i = 0; i <facesArray.length; i++)
             Core.rectangle(inputFrame, facesArray[i].tl(), facesArray[i].br(), new Scalar(0, 255, 0, 255), 3);*/
         return inputFrame.rgba();
