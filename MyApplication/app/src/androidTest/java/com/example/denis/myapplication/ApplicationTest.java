1c7b80e0783c9d9b0fff9aecf695139794137c57
ahed144 <ahed144@gmail.com>
2015-11-04 06:28:32 +0300

    d

--- MyApplication/app/src/androidTest/java/com/example/denis/myapplication/ApplicationTest.java
+++ MyApplication/app/src/androidTest/java/com/example/denis/myapplication/ApplicationTest.java
@@ -1,13 +1,132 @@
 package com.example.denis.myapplication;
 
-import android.app.Application;
-import android.test.ApplicationTestCase;
+import android.app.Activity;
+import android.content.Context;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.graphics.Canvas;
+import android.graphics.Color;
+import android.graphics.Paint;
+import android.graphics.PointF;
+import android.media.FaceDetector;
+import android.media.FaceDetector.Face;
+import android.os.Bundle;
+import android.view.View;
+import android.content.Context;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.media.FaceDetector;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.KeyEvent;
+import android.view.Menu;
+import android.view.MenuItem;
 
+import android.app.Activity;
+import android.os.Bundle;
+import android.view.SurfaceView;
+import android.view.View;
+import android.view.WindowManager;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+
+import org.opencv.android.BaseLoaderCallback;
+import org.opencv.android.CameraBridgeViewBase;
+import org.opencv.android.CameraBridgeViewBase.CvCameraViewFrame;
+import org.opencv.android.CameraBridgeViewBase.CvCameraViewListener2;
+import org.opencv.android.JavaCameraView;
+import org.opencv.android.LoaderCallbackInterface;
+import org.opencv.android.OpenCVLoader;
+import org.opencv.android.Utils;
+import org.opencv.core.*;
+import org.opencv.core.Mat;
+import org.opencv.objdetect.CascadeClassifier;
+import org.w3c.dom.Text;
+
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.InputStream;
+import java.util.HashMap;
+import java.util.Map;
 /**
  * <a href="http://d.android.com/tools/testing/testing_android.html">Testing Fundamentals</a>
  */
-public class ApplicationTest extends ApplicationTestCase<Application> {
-    public ApplicationTest() {
-        super(Application.class);
+public class ApplicationTest  extends Activity {
+
+
+       
+
+
+        public void onCreate(Bundle savedInstanceState) {
+            super.onCreate(savedInstanceState);
+            setContentView(new myView(this));
+        }
+
+        private class myView extends View {
+
+
+            private int imageWidth, imageHeight;
+            private int numberOfFace = 5;
+            private FaceDetector myFaceDetect;
+            private FaceDetector.Face[] myFace;
+            float myEyesDistance;
+            int numberOfFaceDetected = 0;
+            private OpenCvFaceDetector detector;
+            private String text_str;
+
+            Bitmap myBitmap;
+
+
+            //
+            public myView(Context context) {
+                super(context);
+                detector = new OpenCvFaceDetector();
+
+                BitmapFactory.Options BitmapFactoryOptionsbfo = new BitmapFactory.Options();
+                BitmapFactoryOptionsbfo.inPreferredConfig = Bitmap.Config.RGB_565;
+                myBitmap = BitmapFactory.decodeResource(getResources(),
+                        R.drawable.face5, BitmapFactoryOptionsbfo);
+                imageWidth = myBitmap.getWidth();
+                imageHeight = myBitmap.getHeight();
+                myFace = new FaceDetector.Face[numberOfFace];
+                myFaceDetect = new FaceDetector(imageWidth, imageHeight,
+                        numberOfFace);
+                numberOfFaceDetected = myFaceDetect.findFaces(myBitmap, myFace);
+
+
+            }
+
+
+            protected void onDraw(Canvas canvas) {
+
+                canvas.drawBitmap(myBitmap, 0, 0, null);
+
+                Paint myPaint = new Paint();
+                myPaint.setColor(Color.GREEN);
+                myPaint.setStyle(Paint.Style.STROKE);
+                myPaint.setStrokeWidth(3);
+
+                for (int i = 0; i < numberOfFaceDetected; i++) {
+                    FaceDetector.Face face = myFace[i];
+                    PointF myMidPoint = new PointF();
+                    face.getMidPoint(myMidPoint);
+                    myEyesDistance = face.eyesDistance();
+
+                    canvas.drawRect((int) (myMidPoint.x - myEyesDistance * 2),
+                            (int) (myMidPoint.y - myEyesDistance * 2),
+                            (int) (myMidPoint.x + myEyesDistance * 2),
+                            (int) (myMidPoint.y + myEyesDistance * 2), myPaint);
+                }
+                if (numberOfFaceDetected != -1) {
+
+                    String new_text_str = String.valueOf(numberOfFaceDetected);
+                    Log.e("OpenCvFaceDetector", new_text_str);
+                }
+
+            }
+        }
     }
-}
+
+
